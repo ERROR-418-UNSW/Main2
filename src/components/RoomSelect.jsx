3947775370b7e67086c234e5e348c0ccf8bf8694
Max Phillips <max@maxphillipsdev.com>
2021-03-07 10:19:50 +1100

    Testing list rooms function

---
+++ src/components/RoomSelect.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+function RoomSelect({ user }) {
+  // Token starts null
+  const [token, setToken] = useState(null);
+  const [roomName, setRoomName] = useState("");
+  const [rooms, setRooms] = useState([]);
+
+  if (!token) {
+  }
+}
+
+export default RoomSelect;
