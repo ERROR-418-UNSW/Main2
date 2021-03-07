7cf2c5ca00a30cc08826e0188902c598a3e93e84
thefirewood <77281319+thefirewood@users.noreply.github.com>
2021-03-07 09:27:42 +0700

    box and submit edit

--- src/components/RoomSelect.jsx
+++ src/components/RoomSelect.jsx
@@ -57,12 +57,16 @@ function RoomSelect({ user }) {
     return (
       <div>
         <input
+          style="position:absolute; top:20%; left:30%; width:40%; height:5%; color:black"
           type="text"
           name="question"
           value={roomName}
           onChange={(event) => setRoomName(event.target.value)}
         />
-        <button onClick={handleGetToken}>Create room</button>
+        <button 
+          style="position:absolute; top:28%; left:40%; width:20%; color:black"
+          onClick={handleGetToken}>Create room
+        </button>
         {rooms.map((room) => (
           <div>
             <button
