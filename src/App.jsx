1cfc2afa705b7742dbc7b09482c95e92b280affd
thefirewood <77281319+thefirewood@users.noreply.github.com>
2021-03-07 10:56:30 +0700

    header

--- src/App.jsx
+++ src/App.jsx
@@ -25,9 +25,12 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Campfire 🔥</h1>
+      <h1>Campfire 🔥</h1>
+      <header class="u-clearfix u-custom-color-2 u-header u-header" id="sec-fa41"><img src="images/unnamed-2.png" alt="" class="u-image u-image-default u-image-1" data-image-width="512" data-image-height="315"><img src="images/unnamed-2.png" alt="" class="u-image u-image-default u-image-2" data-image-width="512" data-image-height="315"><img src="images/unnamed-2.png" alt="" class="u-image u-image-default u-image-3" data-image-width="512" data-image-height="315"><h2 class="u-text u-text-custom-color-3 u-text-1">The CampFire</h2>
+      <img src="images/1809.m10.i008.n006.p.c25.1072200314-burning-bonfire-animation.gif" alt="" class="u-image u-image-default u-image-4" data-image-width="800" data-image-height="600"><img src="images/7d064a7b2c501b9b3937c1a5941b62be2f51aae51.png" alt="" class="u-image u-image-default u-image-5" data-image-width="1000" data-image-height="1003"><div class="u-custom-color-2 u-shape u-shape-rectangle u-shape-1"></div><img src="images/cb96a23d8522823f7e5e9225d7845f44-wooden-arrow-sign-by-vexels.png" alt="" class="u-image u-image-default u-preserve-proportions u-image-6" data-image-width="512" data-image-height="512"><h3 class="u-text u-text-2">Top Rooms<br>
+        <br>
+      </h3>
       </header>
-
       <main>
         {user ? <RoomSelect user={user.displayName} /> : <Login auth={auth} />}
       </main>
